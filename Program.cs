6bb0c1c29f3e07bc1e19dd4a99605a84bccb65b9
Niroshassithanathan <niroshabalu0107@gmail.com>
2024-02-23 21:34:55 -0800

    add

--- Program.cs
+++ Program.cs
@@ -4,6 +4,23 @@ public class Pattern
 {
     public static void Main()
     {
-        // type the code here
-    }
+        int rows, c = 1, s, i, j;
+        rows=Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Pattern:");
+         for(i = 0; i<rows; i++) 
+         {
+            for(s = 1; s<= rows-i; s++)
+            Console.Write(" ");
+            for(j = 0; j <= i; j++)
+             {
+               if (j == 0||i == 0)
+               c = 1;
+               else
+               c = c*(i-j+1)/j;
+               Console.Write(c + " ");
+            }
+            Console.WriteLine();
+         }
+      }
 }
+
